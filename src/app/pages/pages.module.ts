fbdd4604458c92ff5fe6afcb781060174ce45d27
Dolore Avalos Estrada <dolore_av_es@hotmail.com>
2023-12-13 11:31:23 -0600

    Fin sección 7

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -10,6 +10,7 @@ import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 
 
@@ -18,13 +19,15 @@ import { AppRoutingModule } from '../app-routing.module';
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    AccountSettingsComponent
   ],
   exports: [
     DashboardComponent,
     ProgressComponent,
     Grafica1Component,
-    PagesComponent
+    PagesComponent,
+    AccountSettingsComponent
   ],
   imports: [
     CommonModule,
